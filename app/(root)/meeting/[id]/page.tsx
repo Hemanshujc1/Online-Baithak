1ccb89a2370bdef9b981d19ea31f27c9554545c0
Hemanshujc1 <hemanshujc01@gmail.com>
2025-04-27 00:50:50 +0530

    error handling 1

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -9,11 +9,13 @@ import { useState } from 'react';
 
 const Meeting = ({params:{id} }: { params: { id: string }}) => {
 
-  const {user,isLoaded}=useUser();
-  
+  // const {isLoaded,user}=useUser();
+  const {isLoaded}=useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   const {call, isCallLoading} =useGetCallById(id);
+
   if(!isLoaded || isCallLoading){return <Loader/>;}
+
   return (
     <main className='h-screen w-full'>
       <StreamCall call={call}>
