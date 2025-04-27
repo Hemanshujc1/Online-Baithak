e25ed78a68a60885905676ca802dcc697dd77070
Hemanshujc1 <hemanshujc01@gmail.com>
2025-04-27 16:37:37 +0530

    Update page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,44 +1,44 @@
-"use client"
-
+'use client';
+import { useState } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import { useState } from 'react';
-import Loader from '@/components/Loader/Loader';
+import { Loader } from 'lucide-react';
 import { useGetCallById } from '@/hooks/useGetCallById';
-import Alert from '@/components/Alert/Alert';
-import MeetingRoom from '@/components/MeetingRoom/MeetingRoom';
+// import Alert from '@/components/Alert/Alert';
 import MeetingSetup from '@/components/MeetingSetup/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom/MeetingRoom';
 
-const Meeting = ({ params }: { params: { id: string }}) => {
-  const { id } =params;
-  const {isLoaded,user}=useUser();
+const MeetingPage = ({params:{id} }: { params: { id: string }}) => {
+  // const { isLoaded, user } = useUser();
+    const { isLoaded } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const {call, isCallLoading} =useGetCallById(id);
 
-  if(!isLoaded || isCallLoading){return <Loader/>;}
-  if (!call) return (
-    <p className="text-center text-3xl font-bold text-white">
-      Call Not Found
-    </p>
-  );
-  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
-  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+  if (!isLoaded || isCallLoading) return <Loader />;
 
+  // if (!call) return (
+  //   <p className="text-center text-3xl font-bold text-white">
+  //     Call Not Found
+  //   </p>
+  // );
+  // const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  // if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
 
   return (
-    <main className='h-screen w-full'>
+    <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ?  (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete}/>
-          ): (
-            <MeetingRoom/>
-          )}
+
+        {!isSetupComplete ? (
+          <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+        ) : (
+          <MeetingRoom />
+        )}
         </StreamTheme>
       </StreamCall>
-
     </main>
-  )
-}
+  );
+};
 
-export default Meeting
+export default MeetingPage;
